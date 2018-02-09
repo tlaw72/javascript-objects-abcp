04b4da9dc6f2daa3f07949dedaea6b8c59c3ae69
Troy Lawrence <redirect@gmail.com>
2018-02-09 17:27:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {
   'John Denver': 'Country Road',
+<<<<<<< HEAD
   'Elton John': 'Your Song',
   "Sesame Street": "Elmo's World"
 };
@@ -13,4 +14,17 @@ function updatePlaylist (playlist, artistName, songTitle) {
 function removeFromPlaylist (playlist, artistName) {
  delete playlist[artistName];
   return playlist;
+=======
+  'Elton John': 'Your Song'
+};
+
+function updatePlaylist (object, name, title) {
+  object.name = 'title';
+  return playlist;
+}
+
+function removeFromPlaylist (object, name) {
+ delete object.name;
+ return playlist;
+>>>>>>> 404dd2236fc53c331d4dd9b92e481492fcb4d54a
 }
